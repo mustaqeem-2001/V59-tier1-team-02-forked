24688cc8ca28ab655aecf73eeec847d560b0c2cd
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2026-02-19 14:33:20 +0000

    Feature: Added Date on the header as per the MVP

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,13 @@
 
+const today = new Date();
+
+const day = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const year = today.getFullYear();
+
+const formattedDate = `${day}/${month}/${year}`;
+
+console.log(formattedDate); // Outputs: 18/02/2026
 function Header() {
     return (
       <header className="p-4 flex justify-between items-center border-b border-gray-600">
@@ -10,6 +19,9 @@ function Header() {
             </li>
             <li>
               <a href="/roles" className="hover:text-blue-400 text-center block">SCRUM Roles</a>
+            </li>
+            <li>
+              <p className="hover:text-blue-400 text-center block formmattedDate">{formattedDate}</p>
             </li>
           </ul>
         </nav>
